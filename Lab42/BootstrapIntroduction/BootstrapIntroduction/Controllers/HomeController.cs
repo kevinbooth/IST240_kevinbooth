3caa650af49b38b82a0fe0c3089c8a9071674517
Kevin Booth <kdbooth97@gmail.com>
2018-02-12 14:52:41 -0500

    d
    d

---
+++ Lab42/BootstrapIntroduction/BootstrapIntroduction/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootstrapIntroduction.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Kevin Booth Technologies";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact Us";
+
+            return View();
+        }
+        public ActionResult CalcApp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CalcApp(string ValueA, string ValueB, string Calc)
+        {
+            int a;
+            int b;
+            string calcType = Calc;
+            int answer = 0;
+
+            if (ValueA != "" && ValueB != "")
+            {
+                a = Convert.ToInt32(ValueA);
+                b = Convert.ToInt32(ValueB);
+
+                switch (calcType)
+                {
+                    case "add":
+                        answer = a + b;
+                        break;
+                    case "subtract":
+                        answer = a - b;
+                        break;
+                    case "multiply":
+                        answer = a * b;
+                        break;
+                    case "divide":
+                        answer = a / b;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+            else { return View("CalcApp", (object)"Please enter a number!"); }
+            return View("CalcApp", (object)answer.ToString());
+        }
+    }
+}
