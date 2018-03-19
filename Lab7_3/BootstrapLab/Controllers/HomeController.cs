f737c5eb05441db2e6dd38a3904d8be51db509e2
NHTI <nhti@conc.students.local>
2018-03-19 16:01:52 -0400

    7.3 Push

---
+++ Lab7_3/BootstrapLab/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using BootstrapLab.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootstrapLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Basic()
+        {
+            var person = new Person
+            {
+                FirstName = "Kevin",
+                LastName = "Booth"
+            };
+            return View(person);
+        }
+    }
+}
